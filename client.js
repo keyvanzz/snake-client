0aaa6e5257db047eff897d16edf8bcaf788a96c0
Keyvan <k.kholghi@gmail.com>
2019-09-26 00:43:27 +0000

    Added input.js and export both setupInput and handleUserInput functions to that

--- client.js
+++ client.js
@@ -1,24 +1,32 @@
-//Establishes connection with the game server
 const net = require('net');
 const connect = function () {
     const conn = net.createConnection({
-        host: '192.168.88.254',
+        host: 'localhost',
         port: 50541
     });
-    // interpret incoming data as text
-    conn.setEncoding('utf8');
     conn.on('data', (data) => {
         console.log('Server says: ', data);
     });
-
+    conn.setEncoding('utf8');
     conn.on('connect', () => {
-        console.log("Successfully connected to game server");
-        conn.write(`Name: K1z`)
-    })
-
+        console.log('Successfully connected to game server');
+        conn.write('Name: k1z');
+        // setInterval(() => {
+        //     conn.write('Move: up');
+        // }, 1000);
+        // setInterval(() => {
+        //     conn.write('Move: right');
+        // }, 300);
+        // setInterval(() => {
+        //     conn.write('Move: down');
+        // }, 500);
+        // setInterval(() => {
+        //     conn.write('Move: left');
+        // }, 700);
+        // setInterval(() => {
+        //     conn.write('Move: up');
+        // }, 900);
+    });
     return conn;
 }
-
-module.exports = {
-    connect
-};
+module.exports = { connect }
---
+++ input.js
@@ -0,0 +1,23 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', handleUserInput)
+    return stdin;
+}
+
+const handleUserInput = (key) => {
+    if (key === '\u0003') {
+        console.log(`Terminal session closed`)
+        process.exit();
+    }
+}
+
+module.exports = {
+    setupInput
+};
